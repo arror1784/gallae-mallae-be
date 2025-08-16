31af5dd6b7224a11fdd9aa00465c490e53a1ff39
펭귄교실 <102402670+whitepencle@users.noreply.github.com>
2025-08-17 00:29:40 +0900

    api 기초 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ReceiptsModule } from './receipts/receipts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ReceiptsModule],
   controllers: [AppController],
   providers: [AppService],
 })
